406b3e72a77e124f5875b6a1d0e7ff1462067021
jg <jovanyguia@gmail.com>
2018-11-19 19:06:58 +0100

    Initial commit

---
+++ app/src/main/java/com/jovanyguiagmail/networkcodelabs/MainContrat.java
@@ -0,0 +1,28 @@
+package com.jovanyguiagmail.networkcodelabs;
+
+public interface MainContrat {
+
+    interface View {
+
+        void setPresenter(Presenter presenter);
+
+        void setPosts(Post[] posts);
+
+        void showError(String title, String error);
+
+        void hideError();
+
+        void showNoPostsMessage(boolean showMessage);
+
+    }
+
+    interface Presenter {
+
+        void start();
+
+        void loadPosts();
+
+        void onLoadPostImageError(String error, Exception e);
+
+    }
+}
